b15b2377073c2207e661ea3127a6305a83c78119
Mark Ohnsman <Mark.Ohnsman@gmail.com>
2022-01-06 16:49:20 -0700

    pre-code

---
+++ app.js
@@ -0,0 +1,47 @@
+//region pre-code
+const products = [
+  {
+    name: 'Cozy Boots',
+    description: 'These boots were made for walking, and thats what you\'ll do',
+    imgUrl: '/assets/img/boots.png',
+    quantity: 3
+
+  }, {
+    name: 'Bone Bow',
+    description: 'Ranged attacks, Stay safe use a pointed stick from afar',
+    imgUrl: '/assets/img/bow.png',
+    quantity: 2
+  }, {
+    name: 'Eagle\'s Talon',
+    description: 'Fly through your enemies with this feathered edge.',
+    imgUrl: '/assets/img/dagger.png',
+    quantity: 1
+  }, {
+    name: 'Horned Helm',
+    description: 'A nice horned helm be careful with the color red when wearing',
+    imgUrl: '/assets/img/helm.png',
+    quantity: 4
+  }, {
+    name: 'Rations',
+    description: 'Yum Yum, You are going to need to eat on your adventure. Take this!',
+    imgUrl: '/assets/img/ration.png',
+    quantity: 10
+  }, {
+    name: 'Mysterious Ring',
+    description: 'There is probably some left over magic in this ring purchase it to identify',
+    imgUrl: '/assets/img/ring.png',
+    quantity: 1
+  }, {
+    name: 'Rope',
+    description: 'No adventurer is complete without a bundle of sturdy rope',
+    imgUrl: '/assets/img/rope.png',
+    quantity: 4
+  }, {
+    name: 'Bag of Holding',
+    description: 'Where are you going to keep all of your things without a bag such as this',
+    imgUrl: '/assets/img/bag.png',
+    quantity: 0
+  }
+]
+//#endregion
+
